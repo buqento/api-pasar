c163b0dec88d6a50c2c450775a5fdd0ad1548344
Yusuf Yacob Ona Ola <yusuf.yacob@docotel.com>
2018-11-07 14:21:38 +0700

    new

--- index.php
+++ index.php
@@ -166,22 +166,7 @@ function deletePesanan(){
 
 function getPesanans($id) {
 // $sql = "SELECT * FROM summary_pesanan WHERE pengguna_id=$id";
-// $sql = "SELECT pesanan.id, pesanan.tanggal, pesanan.pengguna_id, pesanan.produk_id, pesanan.jumlah,pesanan.total_bayar, pesanan.keterangan, produk.nama, produk.foto FROM pesanan INNER JOIN produk ON pesanan.produk_id = produk.kode";
-$sql = "SELECT 
-pesanan.id,
-pesanan.tanggal,
-pesanan.pengguna_id,
-pesanan.produk_id,
-pesanan.jumlah,
-Format(pesanan.total_bayar, '##.##0') AS total_bayar,
-pesanan.keterangan,
-produk.nama,
-produk.foto
-FROM pesanan
-INNER JOIN produk
-ON pesanan.produk_id = produk.kode 
-WHERE pesanan.status = 0 AND substr(pesanan.tanggal,1,10)=substr(current_timestamp(),1,10)
-ORDER BY pesanan.tanggal DESC ";
+$sql = "SELECT pesanan.id, pesanan.tanggal, pesanan.pengguna_id, pesanan.produk_id, pesanan.jumlah, Format(pesanan.total_bayar, '##.##0') AS total_bayar, pesanan.keterangan, produk.nama, produk.foto FROM pesanan INNER JOIN produk ON pesanan.produk_id = produk.kode WHERE pesanan.status = 0 AND substr(pesanan.tanggal,1,10)=substr(current_timestamp(),1,10) AND pesanan.pengguna_id=$id ORDER BY pesanan.tanggal DESC ";
   try {
     $db = getDB();
     $stmt = $db->query($sql);
