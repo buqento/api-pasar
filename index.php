e13b591a09ec23427b68713b49e55fe0ebbc50fb
Yusuf Yacob Ona Ola <yusuf.yacob@docotel.com>
2018-11-07 14:06:34 +0700

    new

--- index.php
+++ index.php
@@ -168,7 +168,7 @@ function getPesanans($id) {
 // $sql = "SELECT * FROM summary_pesanan WHERE pengguna_id=$id";
 
 $sql = "SELECT pesanan.id, pesanan.tanggal, pesanan.pengguna_id, pesanan.produk_id, pesanan.jumlah,
-Format(pesanan.total_bayar, '##.##0') AS total_bayar, pesanan.keterangan, produk.nama, produk.foto
+pesanan.total_bayar, pesanan.keterangan, produk.nama, produk.foto
 FROM pesanan INNER JOIN produk
 ON pesanan.produk_id = produk.id 
 WHERE pesanan.status = 0 AND pesanan.pengguna_id=$id 
